added a fix rotation function in li_utils instead of the draw utils

li_utils.py
@@ -180,6 +180,11 @@ def get_projection_product_matricies(P):
 
     return K, R, camera_pos[..., None]
 
+def fix_rotation_matrix(K, R):
+    if K[1,1] < 0:
+        return rotate3d_around_y_180 @ R
+    return R
+
 def product_matricies_to_projection_matrix(K, R, p):
     H = K@R
     camera_matrix = np.hstack((I_3, -p))
@@ -591,13 +596,3 @@ def calibrate_camera(real_points, screen_points):
     P = screen_norm_matrix_inv@P@real_norm_matrix
 
     return P/P[-1,-1]
-
-
-
-
-
-
-
-
-
-

main.py
@@ -88,7 +88,7 @@ def try3():
 
     cv2.destroyAllWindows()
 
-def default():
+def try4():
     DIST = .33
     # mirrored x = -x y = y z = z
     real_points_m = np.array([
@@ -138,6 +138,8 @@ def default():
 
         def display(p):
             internal, rotation, position = li_utils.get_projection_product_matricies(p)
+
+            rotation = li_utils.fix_rotation_matrix(internal, rotation)
             plt = draw_utils.show_scene(real_points_m, internal, rotation, position)
         
         def draw_on_pic(P, booll=False):
@@ -152,7 +154,7 @@ def default():
         draw_on_pic(P, booll=True)
 
 if __name__ == '__main__':
-    default()
+    try4()
 
 
 